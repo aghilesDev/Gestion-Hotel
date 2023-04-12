55e9798ad0827b942ae1e2bb7bc87a311f0a58cb
aghilesDev <aghiles.goumeziane@gmail.com>
2023-04-12 18:06:24 -0400

    instancier et utiliser un objet Hotel dans le programme

--- Program.cs
+++ Program.cs
@@ -5,11 +5,22 @@
         static void Main(string[] args)
         {
             Client client1 = new Client("Mohammed", "Ali", "mohammed.ali@gmail.com", "1234-5678-9012-3456", 101);
-            Console.WriteLine(client1.Nom); 
-            Console.WriteLine(client1.Prenom); 
-            Console.WriteLine(client1.Courriel); 
-            Console.WriteLine(client1.CarteCredit); 
-            Console.WriteLine(client1.NumeroChambre); 
+            Client client2 = new Client("Mohammed", "Ali", "mohammed.ali@gmail.com", "1234-5678-9012-3456", 101);
+
+
+            Hotel hotel1 = new Hotel();
+            hotel1.AjouterClient(client1);
+            hotel1.AjouterClient(client2);
+            foreach (Client client in hotel1.Clients)
+            {
+                Console.Write("Client: ");
+                Console.Write(client1.Nom);
+                Console.Write(client1.Prenom);
+                Console.Write(client1.Courriel);
+                Console.Write(client1.CarteCredit);
+                Console.Write(client1.NumeroChambre);
+                Console.WriteLine("");
+            }
 
         }
     }
